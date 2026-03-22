8f8df9778e00d0044a7c6a52c92dbc787d845329
Nayara Hurtado <hbn0035194@est.univalle.edu>
2026-03-22 05:54:05 -0400

    Conexion con la bdd local

--- server/db.js
+++ server/db.js
@@ -2,9 +2,9 @@ import { createPool } from "mysql2/promise";
 
 // Definición del pool de conexiones
 export const pool = createPool({
-  host: process.env.DB_HOST || "mysql-adoptaarbol.alwaysdata.net",
+  host: process.env.DB_HOST || "localhost",
   port: parseInt(process.env.DB_PORT) || 3306,
-  user: process.env.DB_USER || "431717",
-  password: process.env.DB_PASSWORD || "Adopta123",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "MyBelle123_",
   database: process.env.DB_NAME || "adoptaarbol_database_db",
 });
